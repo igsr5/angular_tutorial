c0618aebf5dd6b9ad86ed046174a3cbc95c55796
Sora Ichigo <ichigo@web-freework.com>
2020-12-29 01:09:49 +0900

    message

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mockheroes'
+import { HEROES } from './mockheroes';
 import {Observable, of } from 'rxjs';
+import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  getHeroes(): Hero[] {
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mockheroes';
 import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,7 @@ import {HeroService} from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,private messageService: MessageService) { }
 
   heroes!: Hero[];
   selectedHero!: Hero;
@@ -20,6 +21,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero :Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   ngOnInit(): void {
     this.getHeroes();
